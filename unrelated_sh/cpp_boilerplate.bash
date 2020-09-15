f0c44c38fcdbeca7896e9ef6a07ec11668ce1282
Anjishnu Mahanta <35238072+anjishnum@users.noreply.github.com>
2020-09-15 15:37:58 +0530

    Update cpp_boilerplate.bash

--- unrelated_sh/cpp_boilerplate.bash
+++ unrelated_sh/cpp_boilerplate.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "Enter file name: "
 read file_name
+a=0
+while [ $a = 0 ]
+do
+        if [ -f ${file_name}.cpp ]; then
+                echo "Enter a different file name (File already exists): "
+                read file_name
+        else
+                a=1
+        fi
+done
 cat <<- EOF > ${file_name}.cpp
 #include<iostream>
 
